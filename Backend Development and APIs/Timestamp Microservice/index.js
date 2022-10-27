07fa33ea112321887b7a181c4eb579d8a17db723
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2022-10-27 12:33:46 -0400

    challenge complete

---
+++ Backend Development and APIs/Timestamp Microservice/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.get("/api/:date?", function (req, res) {
+  if (req.params.date) {
+    let date = new Date(req.params.date);
+    if (date.toString() === "Invalid Date") {
+      date = new Date(parseInt(req.params.date));
+    }
+    if (date.toString() === "Invalid Date") {
+      res.json({error: date.toString()});
+    } else {
+      res.json({unix: date.getTime(), utc: date.toUTCString()});
+    }
+  }
+  else {
+    let date = new Date();
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+  }
+  
+});
+
+
+
+var listener = app.listen(6676, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
